b3a61e01dbfec7395e893151a5eb655a2868059a
dmitry ragozin <dmtryragozin@gmail.com>
2025-05-20 22:30:58 +0300

    feat: счёчтик ингредиентов

--- src/components/ingredients-category/ingredients-category.tsx
+++ src/components/ingredients-category/ingredients-category.tsx
@@ -1,7 +1,9 @@
 import { forwardRef, useMemo } from 'react';
 import { TIngredientsCategoryProps } from './type';
 import { TIngredient } from '@utils-types';
-import { IngredientsCategoryUI } from '../ui/ingredients-category';
+import { IngredientsCategoryUI } from '@ui';
+import { useSelector } from '@store';
+import { getBun, getIngredients } from '@slices';
 
 export const IngredientsCategory = forwardRef<
   HTMLUListElement,
@@ -15,16 +17,18 @@ export const IngredientsCategory = forwardRef<
     ingredients: []
   };
 
+  const constructorIngredients = useSelector(getIngredients);
+  const constructorBun = useSelector(getBun);
+
   const ingredientsCounters = useMemo(() => {
-    const { bun, ingredients } = burgerConstructor;
     const counters: { [key: string]: number } = {};
-    ingredients.forEach((ingredient: TIngredient) => {
+    constructorIngredients.forEach((ingredient: TIngredient) => {
       if (!counters[ingredient._id]) counters[ingredient._id] = 0;
       counters[ingredient._id]++;
     });
-    if (bun) counters[bun._id] = 2;
+    if (constructorBun) counters[constructorBun._id] = 2;
     return counters;
-  }, [burgerConstructor]);
+  }, [constructorIngredients, constructorBun]);
 
   return (
     <IngredientsCategoryUI

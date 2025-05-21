b1192598c6f450b57b7c99d2e8db2d850a93ef8a
dmitry ragozin <dmtryragozin@gmail.com>
2025-05-21 07:55:22 +0300

    feat: редиректы с регистрации и логина

--- src/pages/login/login.tsx
+++ src/pages/login/login.tsx
@@ -2,17 +2,32 @@ import { FC, SyntheticEvent, useState } from 'react';
 import { LoginUI } from '@ui-pages';
 import { useDispatch, useSelector } from '@store';
 import { getAuthError, loginUser } from '@slices';
+import { useLocation, useNavigate } from 'react-router-dom';
 
 export const Login: FC = () => {
+  const navigate = useNavigate();
+  const location = useLocation();
+
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
   const dispatch = useDispatch();
   const error = useSelector(getAuthError);
 
-  const handleSubmit = (e: SyntheticEvent) => {
+  const handleSubmit = async (e: SyntheticEvent) => {
     e.preventDefault();
-    dispatch(loginUser({ email, password }));
+
+    try {
+      const result = await dispatch(loginUser({ email, password })).unwrap();
+
+      if (result.success) {
+        if (location.state?.from && location.state.from !== location.pathname) {
+          navigate(location.state.from ?? '/');
+        } else {
+          navigate('/');
+        }
+      }
+    } catch {}
   };
 
   return (
--- src/pages/register/register.tsx
+++ src/pages/register/register.tsx
@@ -1,9 +1,13 @@
 import { FC, SyntheticEvent, useState } from 'react';
 import { RegisterUI } from '@ui-pages';
 import { useDispatch, useSelector } from '@store';
-import { getAuthError, registerUser } from '@slices';
+import { getAuthError, loginUser, registerUser } from '@slices';
+import { useLocation, useNavigate } from 'react-router-dom';
 
 export const Register: FC = () => {
+  const navigate = useNavigate();
+  const location = useLocation();
+
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
@@ -11,9 +15,22 @@ export const Register: FC = () => {
   const dispatch = useDispatch();
   const error = useSelector(getAuthError);
 
-  const handleSubmit = (e: SyntheticEvent) => {
+  const handleSubmit = async (e: SyntheticEvent) => {
     e.preventDefault();
-    dispatch(registerUser({ name, email, password }));
+
+    try {
+      const result = await dispatch(
+        registerUser({ name, email, password })
+      ).unwrap();
+
+      if (result.success) {
+        if (location.state?.from && location.state.from !== location.pathname) {
+          navigate(location.state.from ?? '/');
+        } else {
+          navigate('/');
+        }
+      }
+    } catch {}
   };
 
   return (
